63bd4ed21de560764cc8a8ddb318b6419c6c4044
Marlos Leles <marlosleles@gmail.com>
2025-02-15 20:34:43 +0000

    Step 1-3 made

---
+++ Exercise3/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+        
+        dog.MakeSound();
+        cat.MakeSound();
+    }
+}
+
+public class Animal()
+{
+    public virtual void MakeSound()
+    {
+        Console.WriteLine("Making a sound");
+    }
+}
+
+public class Dog() : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Bark");
+    }
+}
+
+public class Cat() : Animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("Meow");
+    }
+}
