a3a477de0e2bde4932d65c407f0f2fa3f04706cb
Marlos Leles <marlosleles@gmail.com>
2025-02-20 22:19:16 -0300

    Course finished

---
+++ 2 - Introduction to C#/Classes and Objects/ClassesAndObjectsApp/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = Age;
+    }
+
+    public void Greet()
+    {
+        Console.WriteLine("Hi " + Name);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Person marlos = new Person("Marlos", 30);
+        Person maciel = new Person("Maciel", 30);
+        marlos.Greet();
+        maciel.Greet();
+    }
+}
