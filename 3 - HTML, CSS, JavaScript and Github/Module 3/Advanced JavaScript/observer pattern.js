c5714a4b6b5d864473ab0d8d52923f54c6a3a5b3
Marlos Leles <marlosleles@gmail.com>
2025-05-06 21:39:27 -0300

    Observer pattern feito

---
+++ 3 - HTML, CSS, JavaScript and Github/Module 3/Advanced JavaScript/observer pattern.js
@@ -0,0 +1,34 @@
+class Subject {
+    constructor() {
+        this.observers = [];
+    }
+
+    subscribe(observer) {
+        this.observers.push(observer);
+    }
+
+    unsubscribe(observer) {
+        this.observers = this.observers.filter(obs => obs !== observer);
+    }
+
+    notify() {
+        this.observers.forEach(observer => observer.update());
+    }
+}
+
+Class Observer {
+    constructor(name) {
+        this.name = name;
+    }
+
+    update() {
+        console.log(`${this.name} received notification!`);
+    }
+}
+
+const subject = new Subject();
+const observer1 = new Observer('Observer 1');
+const observer2 = new Observer('Observer 2');
+subject.subscribe(observer1);
+subject.subscribe(observer2);
+subject.notify();
